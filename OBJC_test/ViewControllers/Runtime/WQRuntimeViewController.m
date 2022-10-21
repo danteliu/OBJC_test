0738081c35d1399d19c3202a875e21c7976004f6
dante <1849245841@qq.com>
2022-10-21 10:51:53 +0800

    添加打开scheme的代码

--- OBJC_test/ViewControllers/Runtime/WQRuntimeViewController.m
+++ OBJC_test/ViewControllers/Runtime/WQRuntimeViewController.m
@@ -10,8 +10,10 @@
 
 @interface WQRuntimeViewController ()
 @property (nonatomic, strong) BaseVScrollview *scrollViewMain; /**<  <#属性注释#> */
-@property (nonatomic, strong) UILabel * labelSave;/**<  <#属性注释#> */
-@property (nonatomic, strong) UILabel * labelGet;/**<  <#属性注释#> */
+@property (nonatomic, strong) UILabel *labelSave; /**<  <#属性注释#> */
+@property (nonatomic, strong) UILabel *labelGet; /**<  <#属性注释#> */
+@property (nonatomic, strong) UILabel *labelOpenScheme;/**<  <#属性注释#> */
+@property (nonatomic, strong) NSMutableArray <UIView *> *arrayLables; /**<  <#属性注释#> */
 @end
 
 @implementation WQRuntimeViewController
@@ -30,100 +32,141 @@
 }
 
 - (void)initData {/**<  初始化数据 */
-
-    
-    
 }
 
 - (void)initView {/**<  初始化视图 */
     [self.view addSubview:self.scrollViewMain];
-    
-    [self.scrollViewMain.bgView addSubview:self.labelSave];
-    [self.scrollViewMain.bgView addSubview:self.labelGet];
-    
+
     [self.scrollViewMain mas_makeConstraints:^(MASConstraintMaker *make) {
         make.edges.mas_equalTo(UIEdgeInsetsZero);
     }];
-    [self.labelSave mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.centerX.offset(0);
-        make.top.offset(30);
+    [self.arrayLables enumerateObjectsUsingBlock:^(UIView * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        obj.addTo(self.scrollViewMain.bgView);
+    }];
+    [self.arrayLables mas_distributeViewsAlongAxis:(MASAxisTypeVertical) withFixedSpacing:0 leadSpacing:0 tailSpacing:0];
+    [self.arrayLables mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.right.offset(0);
         make.height.mas_equalTo(44);
     }];
-    [self.labelGet mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.centerX.offset(0);
-        make.top.equalTo(self.labelSave.mas_bottom).offset(0);
-        make.left.right.offset(0);
-        make.height.mas_equalTo(44);
-        make.bottom.offset(0);
-    }];
-
 }
+
 #pragma mark -
 #pragma mark event
 - (void)saveModel {/**<  保存模型 */
     /**<
-     1. stringByAppendingString是字符串拼接，拼接路径时要在名称前加“/”
-     2. stringByAppendingPathComponent是路径拼接，会在字符串前自动添加“/”，成为完整路径
+       1. stringByAppendingString是字符串拼接，拼接路径时要在名称前加“/”
+       2. stringByAppendingPathComponent是路径拼接，会在字符串前自动添加“/”，成为完整路径
      */
     NSString *documentsDir = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
-    NSString *filePath=[documentsDir stringByAppendingPathComponent:@"dante.kkk"];
-    WQTeacher *t=[[WQTeacher alloc] init];
-    t.stringAge=@"14";
-    t.stringName=@"dante";
-    t.stringAge1=@"dd";
-    t.stringAge2=@"dd2";
-    t.stringAge3=@"dd3";
-    WQTeacherType*type= [[WQTeacherType alloc] init];
-    type.stringStar=@"5";
-    t.type=type;
-    
+    NSString *filePath = [documentsDir stringByAppendingPathComponent:@"dante.kkk"];
+    WQTeacher *t = [[WQTeacher alloc] init];
+
+    t.stringAge = @"14";
+    t.stringName = @"dante";
+    t.stringAge1 = @"dd";
+    t.stringAge2 = @"dd2";
+    t.stringAge3 = @"dd3";
+    WQTeacherType *type = [[WQTeacherType alloc] init];
+    type.stringStar = @"5";
+    t.type = type;
+
     [NSKeyedArchiver archiveRootObject:t toFile:filePath];
 }
+
 - (void)getModel {/**<  获取模型 */
     NSString *documentsDir = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
-    NSString *filePath=[documentsDir stringByAppendingPathComponent:@"dante.kkk"];
-    WQTeacher *t =[NSKeyedUnarchiver unarchiveObjectWithFile:filePath];
+    NSString *filePath = [documentsDir stringByAppendingPathComponent:@"dante.kkk"];
+    WQTeacher *t = [NSKeyedUnarchiver unarchiveObjectWithFile:filePath];
+
     Log(t.type.stringStar);
     Log([t mj_keyValues]);
 }
+
 #pragma mark -
 #pragma mark lazy
--(id)scrollViewMain{
+- (id)scrollViewMain {
     if (!_scrollViewMain) {
-        _scrollViewMain=({
+        _scrollViewMain = ({
             BaseVScrollview *obj = [[BaseVScrollview alloc] init];
             obj.infoSv.alwaysBounceVertical = YES;
             obj;
         });
     }
+
     return _scrollViewMain;
 }
--(id)labelSave{
+
+- (id)labelSave {
     if (!_labelSave) {
-        _labelSave=({
-            UILabel *obj=[[UILabel alloc] init];
+        _labelSave = ({
+            UILabel *obj = [[UILabel alloc] init];
             obj.str(@"存").color(@"white").centerAlignment.bgColor(@"random");
-            obj.onClick(^(void){
+            obj.onClick(^(void) {
                 [self saveModel];
             });
             obj;
         });
     }
+
     return _labelSave;
 }
 
--(id)labelGet{
+- (id)labelGet {
     if (!_labelGet) {
-        _labelGet=({
-            UILabel *obj=[[UILabel alloc] init];
+        _labelGet = ({
+            UILabel *obj = [[UILabel alloc] init];
             obj.str(@"取").color(@"white").centerAlignment.bgColor(@"random");
-            obj.onClick(^(void){
+            obj.onClick(^(void) {
                 [self getModel];
             });
             obj;
         });
     }
+
     return _labelGet;
 }
+
+- (id)labelOpenScheme {
+    if (!_labelOpenScheme) {
+        _labelOpenScheme = ({
+            UILabel *obj = [[UILabel alloc] init];
+            obj.str(@"打开北移scheme").color(@"white").centerAlignment.bgColor(@"random");
+            obj.onClick(^(void) {
+                [self openBeijingCmcc];
+            });
+            obj;
+        });
+    }
+
+    return _labelOpenScheme;
+}
+
+- (void)openBeijingCmcc {/**<  <#注释#> */
+//    NSURL *url = [NSURL URLWithString:@"weixin://"];
+    NSURL *url=[NSURL URLWithString:@"cn.10086.app://url=https://app.10086.cn/cmcc-app/flow/flowHome.html"];
+    Log([[UIApplication sharedApplication] canOpenURL:url]);
+    if (@available(iOS 10.0, *)) {
+        [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:^(BOOL success) {
+            Log(@"打开状态:");
+            Log(success);
+        }];
+    } else {
+        [[UIApplication sharedApplication] openURL:url];
+    }
+}
+
+- (id)arrayLables {
+    if (!_arrayLables) {
+        _arrayLables = ({
+            NSMutableArray *obj = [[NSMutableArray alloc] init];
+            [obj addObject:self.labelSave];
+            [obj addObject:self.labelGet];
+            [obj addObject:self.labelOpenScheme];
+            obj;
+        });
+    }
+
+    return _arrayLables;
+}
+
 @end

76329038ce6d25b444457b68d1b14d12d3d046e9
dwhaozhenyu3 <haozhenyu@aspirecn.com>
2023-08-05 01:21:49 +0800

    添加自己的宏定义文件

--- OBJC_test/publicHeader/Define/String_Dfine.h
+++ OBJC_test/publicHeader/Define/String_Dfine.h
@@ -9,6 +9,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+#define DDLog(...) printf("%s %s 第%d行: %s\n",__TIME__, __PRETTY_FUNCTION__ ,__LINE__, [[NSString stringWithFormat:__VA_ARGS__] UTF8String]);
 @interface String_Dfine : NSObject
 extern NSString * const infoKey;
 @end
